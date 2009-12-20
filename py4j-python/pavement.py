91ef3782dada2846415a29279a3b38dd1189b740
Barthelemy Dagenais <bart@cs.mcgill.ca>
2009-12-20 22:14:57 +0000

    Finished work on infrastructure.
    Basically, everything is ready for the release, except the release command itself (I only want to try it when I will release it for good).
    
    The current paver script has a target called big_release that builds everything: java, documentation, python, python setup, python egg. Woohoo!
    
    fixes #5

--- py4j-python/pavement.py
+++ py4j-python/pavement.py
@@ -8,10 +8,18 @@ import paver.doctools
 from paver.setuputils import setup, find_package_data
 from setuptools import find_packages
 
+DOC_DIR = 'doc'
+DIST_DIR = 'dist'
+VERSION = '0.1'
+RELEASE = 'py4j-' + VERSION
+JAR_FILE = 'py4j' + VERSION + '.jar'
+
 setup(
     name="Py4J",
-    packages = ['py4j', 'py4j.tests'],
-    package_dir = {'py4j':'src/py4j'},
+    packages=['','py4j', 'py4j.tests'],
+    package_dir={'':'src'},
+    include_package_data = True,
+    package_data = {'':['*.jar']},
     version="0.1",
     url="http://py4j.sourceforge.net/",
     author="Barthelemy Dagenais",
@@ -44,37 +52,84 @@ def scp_dir(source, dest):
     sh("scp -r %s %s" % (source, dest))
 
 @task
-@needs('generate_setup', 'minilib', 'html', 'setuptools.command.sdist')
+@needs('generate_setup', 'minilib', 'html_complete', 'py4j_java', 'setuptools.command.sdist')
 def sdist():
     """Overrides sdist to make sure that our setup.py is generated."""
     pass
 
 @task
+@needs('py4j_java')
+def set_jar():
+    jar = path('../py4j-java/dist') / RELEASE / JAR_FILE
+    jar.copy('src/' + JAR_FILE)
+
+@task
+@needs('set_jar','setuptools.command.bdist_egg')
+def bdist_egg():
+    pass
+
+@task
+@needs('sdist','bdist_egg','soft_clean')
+def big_release():
+    pass
+
+@task
+@needs('html')
+def html_complete():
+    sh('ant clean-doc', False, False, '../py4j-java')
+    sh('ant javadoc', False, False, '../py4j-java')
+    
+    print('Moving javadoc')
+    # HACK: path must be relative to symlink, not to current directory. Wow...
+    javadoc = path('../py4j-java/javadoc')
+    javadoc.copytree('doc/_static/javadoc')
+
+@task
+def py4j_java():
+    sh('ant clean-dist', False, False, '../py4j-java')
+    sh('ant dist', False, False, '../py4j-java')
+    
+    print('Moving py4j-java')
+    py4j_java = path('../py4j-java/dist') / RELEASE
+    py4j_java.copytree('py4j-java')
+    
+@task
 @needs('paver.doctools.html')
 def html(options):
     """Build the docs and put them into our package."""
-    destdir = path('docs')
+    destdir = path(DOC_DIR)
     destdir.rmtree()
     builtdocs = path("../py4j-web") / options.builddir / "html"
     builtdocs.move(destdir)
     
 @task
-@needs('html')
+@needs('html','javadoc')
 def deploy():
-    docdir = path('docs')
+    docdir = path(DOC_DIR)
     docdir.symlink('htdocs')
-    scp_dir('htdocs','barthe,py4j@web.sourceforge.net:/home/groups/p/py/py4j')
+    scp_dir('htdocs', 'barthe,py4j@web.sourceforge.net:/home/groups/p/py/py4j')
     htdocs = path('htdocs')
     htdocs.remove()
     
 @task
-@needs('paver.doctools.doc_clean','distutils.command.clean',)
+@needs('paver.doctools.doc_clean', 'distutils.command.clean','soft_clean')
 def clean():
-    distdir = path('dist')
-    docdir = path('docs')
+    distdir = path(DIST_DIR)
+    distdir.rmtree()
+    
+@task
+def soft_clean():
+    docdir = path(DOC_DIR)
     build = path('build')
     eggdir = path('Py4J.egg-info')
-    distdir.rmtree()
     docdir.rmtree()
     eggdir.rmtree()
-    build.rmtree()
+    build.rmtree() 
+    py4j_java = path('py4j-java')
+    py4j_java.rmtree()
+    jar = path('src') / JAR_FILE
+    jar.remove()
+    egg_info = path('src/Py4J.egg-info')
+    egg_info.rmtree()
+    
+    
