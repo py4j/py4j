f491760ac0f8d4cbffd944ff551681d9f19b7da2
Barthelemy Dagenais <bart@cs.mcgill.ca>
2010-10-12 23:33:24 +0000

    Initial import.

---
+++ net.sf.py4j.defaultserver/src/net/sf/py4j/defaultserver/DefaultServerActivator.java
@@ -0,0 +1,30 @@
+package net.sf.py4j.defaultserver;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class DefaultServerActivator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		DefaultServerActivator.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		DefaultServerActivator.context = null;
+	}
+
+}
