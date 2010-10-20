12c846b00227d016be96e293ce7a4726220b7ccb
Barthelemy Dagenais <bart@cs.mcgill.ca>
2010-10-20 12:19:02 +0000

    Initial work on py4j update site.

--- net.sf.py4j.defaultserver/src/net/sf/py4j/defaultserver/DefaultServerActivator.java
+++ net.sf.py4j.defaultserver/src/net/sf/py4j/defaultserver/DefaultServerActivator.java
@@ -3,9 +3,15 @@ package net.sf.py4j.defaultserver;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
+import py4j.GatewayServer;
+
 public class DefaultServerActivator implements BundleActivator {
 
 	private static BundleContext context;
+
+	private static DefaultServerActivator activator;
+
+	private GatewayServer server;
 
 	static BundleContext getContext() {
 		return context;
@@ -13,18 +19,38 @@ public class DefaultServerActivator implements BundleActivator {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
 		DefaultServerActivator.context = bundleContext;
+		activator = this;
+		server = new GatewayServer(this);
+		server.start();
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext bundleContext) throws Exception {
-		DefaultServerActivator.context = null;
+		if (server != null) {
+			server.shutdown();
+		}
+		context = null;
+		activator = null;
+	}
+
+	public GatewayServer getServer() {
+		return server;
+	}
+
+	public static DefaultServerActivator getDefault() {
+		return activator;
 	}
 
 }
---
+++ net.sf.py4j.defaultserver/src/net/sf/py4j/defaultserver/DefaultServerStartup.java
@@ -0,0 +1,13 @@
+package net.sf.py4j.defaultserver;
+
+import org.eclipse.ui.IStartup;
+
+
+public class DefaultServerStartup implements IStartup {
+
+	@Override
+	public void earlyStartup() {
+		// Do nothing for now
+	}
+
+}
