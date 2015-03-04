64ee81aca21eb39a2ac137a78ed85c575571be6e
Jonah Graham <jonah@kichwacoders.com>
2015-03-04 19:14:51 +0000

    Doc for members

--- py4j-python/src/py4j/tests/java_help_test.py
+++ py4j-python/src/py4j/tests/java_help_test.py
@@ -72,3 +72,23 @@ def test_help_pattern_2():
             assert_in('Help on class ExampleClass in package py4j.examples', doc)
             assert_not_in('method1', doc)
             assert_in('getField1', doc)
+
+def test_help_method():
+    with example_app_process():
+        with gateway() as g:
+            ex = g.getNewExample()
+            doc = g.help(ex.method7, display=False)
+            # Make sure multiple method7s appear (overloaded method)
+            assert_in('method7(int)', doc)
+            assert_in('method7(Object)', doc)
+            assert_not_in('method1', doc)
+
+def test_doc_method():
+    with example_app_process():
+        with gateway() as g:
+            ex = g.getNewExample()
+            doc = ex.method7.__doc__
+            # Make sure multiple method7s appear (overloaded method)
+            assert_in('method7(int)', doc)
+            assert_in('method7(Object)', doc)
+            assert_not_in('method1', doc)
