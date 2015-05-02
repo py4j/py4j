69b6586ac65a1f3d1736458d5520b610191bb55d
Jonah Graham <jonah@kichwacoders.com>
2015-05-02 22:39:00 +0100

    Remove dependencies on nose, use assert statement directly

--- py4j-python/src/py4j/tests/java_dir_test.py
+++ py4j-python/src/py4j/tests/java_dir_test.py
@@ -6,7 +6,6 @@ from py4j.java_gateway import JavaGateway, GatewayParameters, java_import,\
 from py4j.tests.java_gateway_test import (
     start_example_app_process)
 from contextlib import contextmanager
-from nose.tools import eq_
 
 ExampleClassFields = sorted([
                       "field10",
@@ -70,7 +69,7 @@ def gateway(*args, **kwargs):
     try:
         yield g
         # Call a dummy method to make sure we haven't corrupted the streams
-        eq_(lineSep, g.jvm.System.lineSeparator())
+        assert lineSep == g.jvm.System.lineSeparator()
     finally:
         g.shutdown()
 
@@ -78,26 +77,26 @@ def test_dir_object():
     with example_app_process():
         with gateway() as g:
             ex = g.getNewExample()
-            eq_(sorted(dir(ex)), ExampleClassMethods)
+            assert sorted(dir(ex)) == ExampleClassMethods
 
 def test_dir_object_fields():
     with example_app_process():
         with gateway(auto_field=True) as g:
             ex = g.getNewExample()
-            eq_(sorted(dir(ex)), sorted(ExampleClassMethods + ExampleClassFields))
+            assert sorted(dir(ex)) == sorted(ExampleClassMethods + ExampleClassFields)
 
 def test_dir_object_shows_manually_called_after_dir():
     with example_app_process():
         with gateway() as g:
             ex = g.getNewExample()
-            eq_(sorted(dir(ex)), ExampleClassMethods)
+            assert sorted(dir(ex)) == ExampleClassMethods
             try:
                 ex.does_not_exist_in_example()
                 raise AssertionError("Method should not have succeeded")
             except:
                 pass
             # Make sure the manually called method now shows up
-            eq_(sorted(dir(ex)), sorted(ExampleClassMethods + ['does_not_exist_in_example']))
+            assert sorted(dir(ex)) == sorted(ExampleClassMethods + ['does_not_exist_in_example'])
 
 def test_dir_object_shows_manually_called_before_dir():
     with example_app_process():
@@ -109,24 +108,24 @@ def test_dir_object_shows_manually_called_before_dir():
             except:
                 pass
             # Make sure the manually called method now shows up
-            eq_(sorted(dir(ex)), sorted(ExampleClassMethods + ['does_not_exist_in_example']))
+            assert sorted(dir(ex)) == sorted(ExampleClassMethods + ['does_not_exist_in_example'])
 
 def test_dir_class():
     with example_app_process():
         with gateway() as g:
             exclass = g.jvm.py4j.examples.ExampleClass
-            eq_(sorted(dir(exclass)), ExampleClassStatics)
+            assert sorted(dir(exclass)) == ExampleClassStatics
 
 def helper_dir_jvmview(view):
-    eq_(sorted(dir(view)), [UserHelpAutoCompletion.KEY])
+    assert sorted(dir(view)) == [UserHelpAutoCompletion.KEY]
 
     java_import(view, "com.example.Class1")
     java_import(view, "com.another.Class2")
-    eq_(sorted(dir(view)), [UserHelpAutoCompletion.KEY, "Class1", "Class2"])
-    eq_(sorted(dir(view)), [UserHelpAutoCompletion.KEY, "Class1", "Class2"])
+    assert sorted(dir(view)) == [UserHelpAutoCompletion.KEY, "Class1", "Class2"]
+    assert sorted(dir(view)) == [UserHelpAutoCompletion.KEY, "Class1", "Class2"]
 
     java_import(view, "com.third.Class3")
-    eq_(sorted(dir(view)), [UserHelpAutoCompletion.KEY, "Class1", "Class2", "Class3"])
+    assert sorted(dir(view)) == [UserHelpAutoCompletion.KEY, "Class1", "Class2", "Class3"]
 
 def test_dir_jvmview_default():
     with example_app_process():
@@ -151,12 +150,12 @@ def test_dir_jvmview_two():
             java_import(view1, "com.fourth.Class4")
             java_import(view2, "com.fiftg.Class5")
 
-            eq_(sorted(dir(view1)), [UserHelpAutoCompletion.KEY, "Class1", "Class2", "Class3", "Class4"])
-            eq_(sorted(dir(view2)), [UserHelpAutoCompletion.KEY, "Class1", "Class2", "Class3", "Class5"])
+            assert sorted(dir(view1)) == [UserHelpAutoCompletion.KEY, "Class1", "Class2", "Class3", "Class4"]
+            assert sorted(dir(view2)) == [UserHelpAutoCompletion.KEY, "Class1", "Class2", "Class3", "Class5"]
 
 def test_dir_package():
     with example_app_process():
         with gateway() as g:
-            eq_(sorted(dir(g.jvm)), [UserHelpAutoCompletion.KEY])
-            eq_(sorted(dir(g.jvm.java)), [UserHelpAutoCompletion.KEY])
-            eq_(sorted(dir(g.jvm.java.util)), [UserHelpAutoCompletion.KEY])
+            assert sorted(dir(g.jvm)) == [UserHelpAutoCompletion.KEY]
+            assert sorted(dir(g.jvm.java)) == [UserHelpAutoCompletion.KEY]
+            assert sorted(dir(g.jvm.java.util)) == [UserHelpAutoCompletion.KEY]
