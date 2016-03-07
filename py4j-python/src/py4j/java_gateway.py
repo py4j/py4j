3165fbb28f1ddbd85c9c8571879ce296ccd19e54
Barthelemy Dagenais <barthelemy@infobart.com>
2016-03-07 05:35:38 -0500

    make sure that function exists before calling it (important when java_gateway is already garbage collected. refs #106

--- py4j-python/src/py4j/java_gateway.py
+++ py4j-python/src/py4j/java_gateway.py
@@ -703,6 +703,7 @@ class GatewayConnection(object):
         self.wr = weakref.ref(
             self,
             lambda wr, socket_instance=self.socket:
+            _garbage_collect_connection and
             _garbage_collect_connection(socket_instance))
 
     def start(self):
@@ -867,7 +868,7 @@ class JavaObject(object):
         value = weakref.ref(
             self,
             lambda wr, cc=self._gateway_client, id=self._target_id:
-            _garbage_collect_object(cc, id))
+            _garbage_collect_object and _garbage_collect_object(cc, id))
 
         ThreadSafeFinalizer.add_finalizer(key, value)
 
