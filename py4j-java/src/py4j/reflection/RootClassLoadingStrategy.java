f8dfa680462f8aa0e3a8b62aa45b6bea5fd6f9bd
Barthelemy Dagenais <barthelemy@infobart.com>
2016-03-12 10:59:51 -0500

    Added small unit tests.

--- py4j-java/src/py4j/reflection/RootClassLoadingStrategy.java
+++ py4j-java/src/py4j/reflection/RootClassLoadingStrategy.java
@@ -4,7 +4,7 @@ package py4j.reflection;
  * <p>This class loading strategy just uses the root ClassLoader
  * to load a class from a fully qualified name.</p>
  */
-public class BasicClassLoadingStrategy implements ClassLoadingStrategy {
+public class RootClassLoadingStrategy implements ClassLoadingStrategy {
 
 	@Override
 	public Class<?> classForName(String className) throws
