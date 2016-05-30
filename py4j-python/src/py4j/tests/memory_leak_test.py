87e99b0874111852bc79b583d9f2265122af32ee
Barthelemy Dagenais <barthelemy@infobart.com>
2016-05-30 06:25:57 -0400

    refs #209 -- added callback test

--- py4j-python/src/py4j/tests/memory_leak_test.py
+++ py4j-python/src/py4j/tests/memory_leak_test.py
@@ -5,9 +5,12 @@ import subprocess
 import unittest
 
 from py4j.java_gateway import (
-    JavaGateway, GatewayParameters, DEFAULT_PORT)
+    JavaGateway, GatewayParameters, CallbackServerParameters, DEFAULT_PORT,
+    DEFAULT_PYTHON_PROXY_PORT)
+# TODO rename test_gateway_connection because it is executed as a test
 from py4j.tests.java_gateway_test import (
     PY4J_JAVA_PATH, test_gateway_connection, sleep)
+from py4j.tests.py4j_callback_recursive_example import PythonPing
 
 
 def start_instrumented_gateway_server():
@@ -66,5 +69,42 @@ class GatewayServerTest(unittest.TestCase):
             # InstrumentedObject
             self.assertEqual(4, len(createdSet))
             self.assertEqual(4, len(finalizedSet))
+            self.assertEqual(createdSet, finalizedSet)
+            gateway.shutdown()
+
+    def testPythonToJavaToPython(self):
+        def play_with_ping(gateway):
+            ping = PythonPing()
+            pingpong = gateway.jvm.py4j.examples.PingPong()
+            total = pingpong.start(ping)
+            return total
+
+        def internal_work():
+            gateway2 = JavaGateway(
+                gateway_parameters=GatewayParameters(
+                    port=DEFAULT_PORT+5),
+                callback_server_parameters=CallbackServerParameters(
+                    port=DEFAULT_PYTHON_PROXY_PORT+5))
+            sleep()
+            play_with_ping(gateway2)
+            gc.collect()
+            sleep()
+            gateway2.shutdown()
+
+        with gateway_server_example_app_process():
+            gateway = JavaGateway()
+            gateway.entry_point.startServer2()
+            internal_work()
+            gateway.jvm.py4j.instrumented.MetricRegistry.forceFinalization()
+            sleep()
+            createdSet = gateway.jvm.py4j.instrumented.MetricRegistry.\
+                getCreatedObjectsKeySet()
+            finalizedSet = gateway.jvm.py4j.instrumented.MetricRegistry.\
+                getFinalizedObjectsKeySet()
+
+            # 9 objects: GatewayServer, X GatewayConnection, CallbackClient,
+            # X CallbackConnection, TODO
+            self.assertEqual(9, len(createdSet))
+            self.assertEqual(9, len(finalizedSet))
             self.assertEqual(createdSet, finalizedSet)
             gateway.shutdown()
