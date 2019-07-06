472c7a632d4f126c18acd857b63c5f5aa1390fc4
David A. Hannasch <David.A.Hannasch@gmail.com>
2019-07-06 06:06:46 -0400

    fixes #345 - try collections.abc before collections to support Python 3.7

--- py4j-python/src/py4j/java_collections.py
+++ py4j-python/src/py4j/java_collections.py
@@ -10,9 +10,19 @@ Created on Jan 22, 2010
 """
 from __future__ import unicode_literals, absolute_import
 
-from collections import (
-    MutableMapping, Sequence, MutableSequence,
-    MutableSet, Set)
+# As of Python 3.3, the abstract base classes in the collections module have
+# been moved to collections.abc.
+# (see https://docs.python.org/3.3/library/collections.abc.html)
+try:
+    # Python >=3.3
+    from collections.abc import (
+        MutableMapping, Sequence, MutableSequence,
+        MutableSet, Set)
+except ImportError:
+    # Python <=3.2
+    from collections import (
+        MutableMapping, Sequence, MutableSequence,
+        MutableSet, Set)
 import sys
 
 from py4j.compat import (
